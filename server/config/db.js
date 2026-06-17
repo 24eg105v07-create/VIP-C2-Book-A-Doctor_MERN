b9079b18e14bf45ccb892292ae6f5a82aee4cbd9
Harish Chethula <24eg105v07@anurag.edu.in>
2026-06-18 01:18:43 +0530

    Fix MongoDB URI

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(
-      `MongoDB Connected: ${conn.connection.host}`
-    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error.message);
+    console.log("MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
